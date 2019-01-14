eab9b3cbfa0c1902b907249f7519136a42f61e10
Paul <paul.ayling@bglgroup.co.uk>
2019-01-14 13:52:14 +0000

    Added Oystercard class and basic test

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,11 @@
+require 'oystercard'
+
 describe Oystercard do
+
+  describe '#New' do
+    it "Should initialize a Oystercard object with a default balance of 0" do
+    expect(subject.balance).to eq 0
+    end
+  end
 
 end
