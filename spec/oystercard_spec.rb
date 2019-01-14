47e2a96ee3e7ef88612bf59a94f29329b18b44be
Sebastian Lavender <sebastianlavender@outlook.com>
2019-01-14 13:58:50 +0000

    add top_up method and tests

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,10 +2,17 @@ require 'oystercard'
 
 describe Oystercard do
 
-  describe '#New' do
+  describe '#balance' do
+
     it "Should initialize a Oystercard object with a default balance of 0" do
     expect(subject.balance).to eq 0
     end
+
+    it "should increment the balance by a number when top_up is called" do
+    subject.top_up(10)
+    expect(subject.balance).to eq 10
+    end
+
   end
 
 end
